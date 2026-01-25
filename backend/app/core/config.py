aa21462a875369d5ceb818c5bda0e449cac0e16f
FostDull <jhon_villac@hotmail.com>
2026-01-25 00:18:50 -0500

    stripe

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,10 @@
 import os
 from dotenv import load_dotenv
+import stripe
 
 load_dotenv()
 
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
+
+stripe.api_key = STRIPE_SECRET_KEY
